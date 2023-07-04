4dda75afdad3de866feded1fc4936d8850f2a2df
Nikita Ryazanov <ryazanov.na@yandex.ru>
2023-07-04 23:12:47 +0400

    Steps 3 and 4: Created a makefile, added help using the picocli library, added the -h flag for help

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,37 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.io.File;
+
+@Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
+class App implements Runnable {
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "Soutput format [default: stylish]")
+    private String format;
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    private boolean help;
+
+    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
+    private boolean version;
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private File filepath1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private File filepath2;
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 
+    @Override
+    public void run() {
+
+    }
 }
