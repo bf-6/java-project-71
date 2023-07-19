e10b3873b5d703b361c8d54e179150b7e257143a
Nikita Ryazanov <ryazanov.na@yandex.ru>
2023-07-19 18:36:54 +0400

    Changes by comments

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,7 +1,6 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
